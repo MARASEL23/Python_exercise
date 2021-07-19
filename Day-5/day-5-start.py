48d88e3ede7c5ee2c9a945beb428d1c041dc075d
Yusuf Öz <yusufoz230644@gmail.com>
2021-07-19 13:02:29 +0300

    .

--- Day-5/day-5-start.py
+++ Day-5/day-5-start.py
@@ -1,6 +1,15 @@
-fruits = ["Apple", "Peach", "Pear"]
+"""fruits = ["Apple", "Peach", "Pear"]
 
 for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
 print(fruits)
+*****************
+# For loop with Range
+for number in range(1, 11, 2):
+    print(number)
+"""
+total = 0
+for number in range(1,101):
+    total += number
+print(total)
