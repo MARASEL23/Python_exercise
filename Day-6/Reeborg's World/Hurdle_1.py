96b766934abbdb30bf39f6427b764a40f39b7d52
Yusuf Öz <yusufoz230644@gmail.com>
2021-08-02 18:58:42 +0300

    .

---
+++ Day-6/Reeborg's World/Hurdle_1.py
@@ -0,0 +1,20 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def turn_tour():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+for step in range(6):
+    turn_tour()
+
---
+++ Day-6/Reeborg's World/drav-square.py
@@ -0,0 +1,13 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def square():
+    turn_right()
+    move()
+turn_left()
+move()
+square()
+square()
+square()
+
